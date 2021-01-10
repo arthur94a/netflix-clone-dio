a9445eae3d4dd04a3ef235c1a94eac1252ba0758
arthur94a <arthuraraujo94@hotmail.com>
2021-01-10 20:21:24 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function infoPage(){
+    window.open("https://www.themoviedb.org/tv/79141");
+}
+
+function watch(){
+    window.open("https://www.netflix.com/browse");
+}
+
+function iOne(){
+    window.open("https://www.themoviedb.org/movie/135397-jurassic-world");
+}
+
+function iTwo(){
+    window.open("https://www.themoviedb.org/tv/82257-cannon-busters");
+}
+
+function iThree(){
+    window.open("https://www.themoviedb.org/tv/246-avatar-the-last-airbender");
+}
+
+function iFour(){
+    window.open("https://www.themoviedb.org/tv/44217-vikings");
+}
+
+function iFive(){
+    window.open("https://www.themoviedb.org/tv/73021-disenchantment");
+}
+
+function iSix(){
+    window.open("https://www.themoviedb.org/tv/71912-the-witcher");
+}
+
+function iSeven(){
+    window.open("https://www.themoviedb.org/movie/122-the-lord-of-the-rings-the-return-of-the-king")
+}
+
+function iEight(){
+    window.open("https://www.themoviedb.org/tv/48891-brooklyn-nine-nine")
+}
+
+function iNine(){
+    window.open("https://www.themoviedb.org/tv/60625-rick-and-morty")
+}
+
+function iTen(){
+    window.open("https://www.themoviedb.org/tv/74387-final-space")
+}
